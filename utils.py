b2f74a8fa46b5bbe5eb6c27680365630979154b5
Sebbyraft <alessandroseba1995@gmail.com>
2021-03-26 15:54:08 +0100

    ⚙ Utilities

---
+++ utils.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+
+def plot_dataset(batch_speckle, batch_clean):
+    n = batch_speckle.shape[0]
+    fig, axes = plt.subplots(
+        nrows = 2*n,
+        ncols = 2, 
+        figsize = (8, 2*n*4))
+
+    for i in range(n):
+        axes[2*i,0].imshow(batch_speckle[i,...,0], cmap='gray')
+        axes[2*i,0].set_title('Input with speckle')
+        axes[2*i,1].imshow(batch_clean[i,...,0], cmap='gray')
+        axes[2*i,1].set_title('Ground truth')
+
+        axes[2*i+1,0].hist(batch_speckle[i,...,0].flatten(), bins=20, histtype='step')
+        axes[2*i+1,0].set_title('Input with speckle')
+        axes[2*i+1,1].hist(batch_clean[i,...,0].flatten(), bins=20, histtype='step')
+        axes[2*i+1,1].set_title('Ground truth')
+
+    plt.show()
